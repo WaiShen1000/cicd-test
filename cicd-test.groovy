3a35f93382b7fb6b74a6785654c4a31a65876aa3
Vincent Tang <vincenttang@varadise.cloud>
2024-02-20 11:32:39 +0800

    c

--- cicd-test.groovy
+++ cicd-test.groovy
@@ -27,8 +27,10 @@ pipeline {
         GenericTrigger(
             genericVariables:[
                     [key: 'project', value:"\$.repository.name", defaultValue:''],
-                    [key: 'tag', value:"\$.ref", defaultValue:''],
                     [key: 'object_kind', value: '\$.ref_type', defaultValue:'']
+                    [key: 'tag', value:"\$.ref", defaultValue:''],
+                    [key: 'pull_request', value: '\$.action', defaultValue:''],
+                    [key: 'pull_request_url', value: '\$.pull_request_url', defaultValue:''],
                 ],
                 causeString: 'Triggered By Gitlab On $tag',
                 genericRequestVariables: [],
@@ -43,6 +45,32 @@ pipeline {
                 regexpFilterExpression: '(^tag\\s\\d+\\.\\d+\\.\\d+$)'
         )
     }
+    // triggers {
+    //     GenericTrigger(
+    //         genericVariables:[
+    //                 [key: 'project', value:"\$.project.name", defaultValue:''],
+    //                 [key: 'tag', value:"\$.ref", regexpFilter:'^[^\\/]+\\/([^\\/]+)\\/', defaultValue:''],
+    //                 [key: 'checkout_sha', value: '\$.checkout_sha', defaultValue:''],
+    //                 [key: 'commit', value:"\$.commits.message", defaultValue:''],
+    //                 [key: 'object_kind', value: '\$.object_kind', defaultValue:''],
+    //                 [key: 'last_commit', value: '\$.object_attributes.last_commit.id', defaultValue:''],
+    //                 [key: 'target_branch', value: '\$.object_attributes.target_branch', defaultValue:''],
+    //                 [key: 'merge_status', value: '\$.object_attributes.detailed_merge_status', defaultValue:''],
+    //                 [key: 'merge_action', value: '\$.object_attributes.action', defaultValue:''],
+    //             ],
+    //             causeString: 'Triggered By Gitlab On $tag',
+    //             genericRequestVariables: [],
+    //             genericHeaderVariables: [],
+    //             token: "gahsia8xuQuaeceighie9doo",
+    //             tokenCredentialId: '',
+    //             printContributedVariables: false,
+    //             printPostContent: false,
+    //             silentResponse: false,
+    //             shouldNotFlattern: false,
+    //             regexpFilterText: '$object_kind $tag $checkout_sha $last_commit $target_branch $merge_status $merge_action',
+    //             regexpFilterExpression: '(^tag_push\\s\\d+\\.\\d+\\.\\d+-alpha[.-]?\\d*\\s.{40}\\s\\s\\s\\s$)|(^merge_request\\s\\s\\s.{40}\\sdev\\smergeable\\sopen$)'
+    //     )
+    // }
     
     stages {
         stage('Hello') {
