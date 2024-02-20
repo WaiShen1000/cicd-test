6bb585da079baf9f9bd2a255160bd835bc6f47b7
Vincent Tang <vincenttang@varadise.cloud>
2024-02-20 10:36:35 +0800

    a

--- cicd-test.groovy
+++ cicd-test.groovy
@@ -65,7 +65,7 @@ pipeline {
     stages {
         stage('Hello') {
             steps {
-                echo 'Hello World'
+                echo "$project - $object_kind $tag"
             }
         }
     }
